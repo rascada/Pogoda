534ae4de4844637ddfcdf39f570bfef3ac1af6ea
marcinlacina <marcin.lacina@windowslive.com>
2015-01-15 00:02:41 +0100

    php po raz drugi

--- index.php
+++ index.php
@@ -254,7 +254,7 @@ $nowdate = date("H:i:s");
 $ostatni = intval($dir['time']);
 $now = intval($nowdate);
 
-if($ostatni>$now-360) $online="<span style='color: darkgreen;'>Stacja jest online!</span>";
+if($ostatni>$now-360 && $iledzis>0) $online="<span style='color: darkgreen;'>Stacja jest online!</span>";
 
 echo $dir['date']." ".$dir['time']."|".$dir['atemp']."|".$dir['wilgo']."|".$dir['cisnie']."|".$dir['srtemp']."|".$dir['podmuch']."|".$dir['swind']."|".$dir['dirwind']."|".$dir['domdirwind']."|".$dir['otemp']."|".$dir['bfw']."|".$dir['dobopad']."|".$dir['deszcz']."|".$dir['tencisn']."  ".$dir['tencisnval']."hPa/h |".$dir['tentemp']." ".$dir['tentempval']."°C/h |".$dir['progno']."|".$dir['zamb']."|".$dir['dew']."°C"."|".$iledzis."|".$online;
 ?>
