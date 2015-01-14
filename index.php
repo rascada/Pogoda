0b915f371a66dfd1f478454d0252041be7eaf25a
marcinlacina <marcin.lacina@windowslive.com>
2015-01-14 20:36:38 +0100

    a tak bedzie lepiej?

--- index.php
+++ index.php
@@ -142,10 +142,10 @@ mysql_select_db($database);
               <div id='strz1' class='strz'></div>
               <div class='dot'></div>
               <div class='liczPods' id='liczPods1'></div>  
-                <span class='liczCyf' id='wSpeed' onclick="akt_wind_click()">Aktulany<br/>1 m/s</span>
+                <span class='liczCyf' id='wSpeed'>Aktulany<br/>1 m/s</span>
             </div>  
         </div><br/>  
-        <span class="compassSpan"  id="aktDirVal">Aktualny</span>
+        <span class="compassSpan"  id="aktDirVal" onclick="akt_wind_click()">Aktualny</span>
     </div>  
     <!-----------------------compass2---------------------------------->
     <div class='compassCont'>
@@ -157,10 +157,10 @@ mysql_select_db($database);
               <div id='strz2' class='strz'></div>
               <div class='dot'></div>
               <div class='liczPods' id='liczPods2'></div>  
-                <span class='liczCyf' id='wPSpeed' onclick="pod_wind_click()">Aktulany<br/>0 m/s</span>
+                <span class='liczCyf' id='wPSpeed'>Aktulany<br/>0 m/s</span>
             </div>
         </div><br/>
-        <span class="compassSpan"  id="domDirVal">Dominujący</span>
+        <span class="compassSpan"  id="domDirVal" onclick="pod_wind_click()">Dominujący</span>
     </div> <br/>
 </div><br/>
     <!----------------------------ciśnienie-------------------------------------------->
