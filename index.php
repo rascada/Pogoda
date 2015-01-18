36611ffa56559d27a7e9cf1890c2aab3ba40bf16
marcinlacina <marcin.lacina@windowslive.com>
2015-01-18 01:25:29 +0100

    Usuwanie debuggera

--- index.php
+++ index.php
@@ -318,8 +318,6 @@ $nowSec = $no[0]+$no[1]+$noS[2];
 
 
 if($ostSec>$nowSec-360 && $iledzis>0) $online="<span style='color: darkgreen;'>Stacja jest online!</span>";
-
-echo $ostSec."  ".$nowSec;
 echo $dir['date']." ".$dir['time']."|".$dir['atemp']."|".$dir['wilgo']."|".$dir['cisnie']."|".$dir['srtemp']."|".$dir['podmuch']."|".$dir['swind']."|".$dir['dirwind']."|".$dir['domdirwind']."|".$dir['otemp']."|".$dir['bfw']."|".$dir['dobopad']."|".$dir['deszcz']."|".$dir['tencisn']."  ".$dir['tencisnval']."hPa/h |".$dir['tentemp']." ".$dir['tentempval']."°C/h |".$dir['progno']."|".$dir['zamb']."|".$dir['dew']."°C"."|".$iledzis."|".$online;
 ?>
 </div>
