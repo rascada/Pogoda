668305a2180ca78f19d4f84cb6ba5d5d5ebe1a36
Damian <carftjr@gmail.com>
2015-01-27 11:25:57 +0100

    Raport dnia
    Poprawki

--- js/script.js
+++ js/script.js
@@ -121,20 +121,18 @@ setTimeout(function() {refdayrep(false);}, 1000);
 
 
 function bottomPanel(){
-    if(width>1500){       
+    if(width>1700){       
         $('#panel1').css('flex-direction','column');        
         $('#panel1').css('flex-grow','0');        
         $('#panel1').css('order','4');
         $('#panel1').css('min-width','0');
         $('#raportDnia').css('flex-direction','column');  
-        $('.RaportArrow').css('top','44%'); 
     }else{
         $('#panel1').css('flex-direction','row');
         $('#panel1').css('flex-grow','1');
         $('#panel1').css('order','-2');
         $('#panel1').css('min-width','90%');
         $('#raportDnia').css('flex-direction','row');
-        $('.RaportArrow').css('top','41%'); 
     }   
 }
 var hint=false;
