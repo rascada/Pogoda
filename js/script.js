f4238d1459701cdab892a9bae82863b116ecb52a
marcinlacina <marcin.lacina@windowslive.com>
2015-01-28 09:40:47 +0100

    kierunki

--- js/script.js
+++ js/script.js
@@ -415,14 +415,14 @@ dzien = document.getElementById("dayrep_k").innerHTML.split("|");
 		document.getElementById("clouds").innerHTML=dzien[17]+"m";
 		
 		var jaki_dzien;
-		if(dzien[3]<20 || dzien[3]>=315) jaki_dzien="z północy";
-		else if(dzien[3]>=20 && dzien[3]<70) jaki_dzien="z północnego wschodu";
-		else if(dzien[3]>=70 && dzien[3]<110) jaki_dzien="ze wschodu";
-		else if(dzien[3]>=110 && dzien[3]<160) jaki_dzien="z południowego wschodu";
-		else if(dzien[3]>=160 && dzien[3]<215) jaki_dzien="z południa";
-		else if(dzien[3]>=215 && dzien[3]<240) jaki_dzien="z południowego zachodu"
-		else if(dzien[3]>=240 && dzien[3]<285) jaki_dzien="z zachodu";
-		else if(dzien[3]>=285 && dzien[3]<315) jaki_dzien="z północego zachodu";
+		if(dzien[3]<20 || dzien[3]>=315) jaki_dzien="z N";
+		else if(dzien[3]>=20 && dzien[3]<70) jaki_dzien="z NE";
+		else if(dzien[3]>=70 && dzien[3]<110) jaki_dzien="z E";
+		else if(dzien[3]>=110 && dzien[3]<160) jaki_dzien="z SE";
+		else if(dzien[3]>=160 && dzien[3]<215) jaki_dzien="z E";
+		else if(dzien[3]>=215 && dzien[3]<240) jaki_dzien="z WE"
+		else if(dzien[3]>=240 && dzien[3]<285) jaki_dzien="z W";
+		else if(dzien[3]>=285 && dzien[3]<315) jaki_dzien="z NW";
 		document.getElementById("windDayDom").innerHTML=jaki_dzien;
 		
 		if( (dzien[16].indexOf("Mozilla")==-1 && dzien[16].indexOf("Chrome")==-1) || dzien[16].indexOf("Trident")!=-1) alert('Używasz niewspieranej przez tę stronę przeglądarki. Zalecamy używanie Chrome lub Firefox w najnowszej wersji.');
