ab2349b248438b1e46e82c08aa1bb8c3ad9c2d5a
Damian <carftjr@gmail.com>
2015-05-14 19:35:45 +0200

    Wybieranie danych z podstawowe i wyrzucanie na stronę

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
-$.getJSON('json/podstawowe.json',function(data){
-	
-});
+var podstawowe = [
+	"podstawowe",
+
+	"temperatureCurrent",
+	"temperatureReal",
+	"temperatureDewPoint",
+	"temperatureTrend",
+	"temperatureAvg",
+
+	"humidity",
+
+	"pressureCurrent",
+	"pressureTrend",
+
+	"rainCurrent",
+	"rainSum",
+
+	"windCurrentSpeed",
+	"windCurrentDir",
+	"windGustSpeed",
+	"windGustDir"
+];
+
+(function(){
+	$.getJSON('json/podstawowe.json', function(data){
+	var i = -1;
+		$.map(data,function(val){
+			if(typeof val === "object"){
+				$.map(val,function(val){
+					i++;changeText(podstawowe[i],val);
+				});
+			}else{
+				i++;changeText(podstawowe[i],val);
+			}
+		});
+	});
+}());
+
+function changeText(id, val){
+	console.log(id,val);
+	document.getElementById(id).innerHTML = val.toString();
+}
