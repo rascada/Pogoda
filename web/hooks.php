e6c51e38bd72eba74854c94d67b0208b1babae3d
marcinlacina <marcin.lacina@zslt.klucznet.pl>
2015-12-13 00:00:11 +0100

    fixes in hooks

--- web/hooks.php
+++ web/hooks.php
@@ -1,8 +1,8 @@
 <?php
 if( $_SERVER['REQUEST_METHOD'] != 'POST' ) {
-    header("HTTP/1.1 403 Forbidden"); echo "Not allowed"; exit;
+    header("Location: /hooks_stat.html"); exit;
 }
 
 $pathToHook = __DIR__."/../env/hooks.sh";
 echo `$pathToHook > /dev/null 2>/dev/null &`.PHP_EOL;
-echo "Deploy running. Check status on ".$_SERVER['SERVER_NAME']."/hooks_stat.html";
+echo "Deploy running. Check status on ".$_SERVER['HTTP_HOST']."/hooks_stat.html";
