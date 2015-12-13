b4a8caf42ad0c2e0b642b79ccebf2b53d7631f97
marcinlacina <marcin.lacina@zslt.klucznet.pl>
2015-12-13 09:10:05 +0100

    fix in front.sh for hooks execution

--- env/front.sh
+++ env/front.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-jade $DIR/../src/Rascada/views/*.jade -o $DIR/../web
-stylus $DIR/../src/Rascada/styl -o $DIR/../web/css
-babel $DIR/../src/Rascada/script --out-dir $DIR/../web/js
+node /usr/local/lib/node_modules/jade/bin/jade.js $DIR/../src/Rascada/views/*.jade -o $DIR/../web
+node /usr/local/lib/node_modules/stylus/bin/stylus $DIR/../src/Rascada/styl -o $DIR/../web/css
+node /usr/local/lib/node_modules/babel-cli/bin/babel.js $DIR/../src/Rascada/script --out-dir $DIR/../web/js
