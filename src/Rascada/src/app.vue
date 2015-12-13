d4f46fab9bce94882b8dd59905fb3cd5598de51e
Damian <carftjr@gmail.com>
2015-12-13 23:06:40 +0100

    style body

--- src/Rascada/src/app.vue
+++ src/Rascada/src/app.vue
@@ -1,3 +1,14 @@
+<style lang="stylus">
+@import url("http://fonts.googleapis.com/css?family=Rajdhani:400,600&amp;subset=latin,latin-ext");
+
+body
+	margin 1em 1.5em
+	overflow-x hidden
+	background #539DC2
+	font-family 'Rajdhani', bold, sans-serif
+  
+</style>
+
 <template lang="jade">
 termometer
 </template>
