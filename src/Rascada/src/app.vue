045bc28cda8a2b0d9dc46634f7231a58ef220e79
Damian <carftjr@gmail.com>
2015-12-14 23:12:15 +0100

    navigationBar component

--- src/Rascada/src/app.vue
+++ src/Rascada/src/app.vue
@@ -1,29 +1,29 @@
 <style lang="stylus">
-@import url("http://fonts.googleapis.com/css?family=Rajdhani:400,600&amp;subset=latin,latin-ext");
+	@import url("http://fonts.googleapis.com/css?family=Rajdhani:400,600&amp;subset=latin,latin-ext");
 
-body
-	margin 1em 1.5em
-	overflow-x hidden
-	background #539DC2
-	font-family 'Rajdhani', bold, sans-serif
-  
+	body
+		margin 1em 1.5em
+		overflow-x hidden
+		background #539DC2
+		font-family 'Rajdhani', bold, sans-serif
+
 </style>
 
 <template lang="jade">
+
+navigationbar(name='Pogoda Skałągi')
 termometer
+
 </template>
 
 <script>
-import termometer from './components/termometer.vue'
+	import termometer from './components/termometer.vue'
+	import navigationbar from './components/navigationBar.vue'
 
-export default {
-  data () {
-    return {
-
-    }
-  },
-  components: {
-    termometer
-  }
-}
+	export default {
+	  components: {
+	    termometer,
+			navigationbar
+	  }
+	}
 </script>
---
+++ src/Rascada/src/components/navigationBar.vue
@@ -0,0 +1,23 @@
+<template lang='jade'>
+  header
+    h1 {{ name }}
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        name: 'Wstaw nazwe'
+      }
+    },
+    props: ['name']
+  }
+</script>
+
+<style lang='stylus'>
+  header
+    background #eee
+
+    margin .5em
+    padding .5em 1em
+</style>
