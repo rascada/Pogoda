19d2acdba3a818c4f1ebcbd55e8b58335c544fda
marcinlacina <marcin.lacina@zslt.klucznet.pl>
2015-12-14 20:41:31 +0100

    create YearController, route and api viewer templates with short descriptions

---
+++ src/SyntaxError/ApiBundle/Controller/YearController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace SyntaxError\ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use SyntaxError\ApiBundle\Tools\Jsoner;
+
+class YearController extends Controller
+{
+    public function recordsAction(Request $request)
+    {
+        $dateTime = new \DateTime(
+            $request->query->has('date') ? $request->query->get('date')." 00:00:00" : 'now'
+        );
+        $request->query->remove('date');
+        $day = $this->get('syntax_error_api.year');
+        $data = [];
+        if( !$request->query->count() ) {
+            foreach(get_class_methods('SyntaxError\ApiBundle\Service\MonthService') as $method) {
+                if( preg_match('/create/', $method) ) {
+                    $key = strtolower( str_replace('create', '', $method) );
+                    $data[$key] = call_user_func_array([$day, $method], [$dateTime]);
+                }
+            }
+        }
+
+        foreach($request->query->all() as $key => $param) {
+            $methodName = "create".ucfirst($key);
+            if( method_exists($day, $methodName) ) {
+                $data[$key] = call_user_func_array([$day, $methodName], [$dateTime]);
+            }
+        }
+        $jsoner = new Jsoner();
+        $jsoner->createJson($data);
+
+        return $request->isXmlHttpRequest() ? $jsoner->createResponse() : $this->render(
+            "SyntaxErrorApiBundle:Year:records.html.twig", [
+            'title' => 'Year records: '.$dateTime->format("Y") ,
+            'json' => $jsoner->getJsonString()
+        ]);
+    }
+
+    public function chartsAction(Request $request, $type)
+    {
+        $dateTime = new \DateTime(
+            $request->query->has('date') ? $request->query->get('date')." 00:00:00" : 'now'
+        );
+        $month = $this->get('syntax_error_api.year');
+
+        $jsoner = new Jsoner();
+        $jsoner->createJson( $month->highDoubleFormatter($dateTime, ucfirst(strtolower($type)) ) );
+
+        return $request->isXmlHttpRequest() ? $jsoner->createResponse() : $this->render(
+            "SyntaxErrorApiBundle:Year:charts.html.twig", [
+            'title' => 'Year '.ucfirst($type).": ".$dateTime->format("Y"),
+            'json' => $jsoner->getJsonString()
+        ]);
+    }
+}
