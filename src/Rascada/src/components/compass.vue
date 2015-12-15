891a0e37610a9778d192b2d3a800323b1fc39b5e
Damian <carftjr@gmail.com>
2015-12-15 18:20:17 +0100

    port compass to vue component

---
+++ src/Rascada/src/components/compass.vue
@@ -0,0 +1,23 @@
+<template lang='jade'>
+  .compass
+    .arrow
+</template>
+
+<style lang='stylus'>
+  @import "~styles/main"
+  @import "~styles/flex"
+
+  .compass
+    @extend .blockShadow, .center
+    width 13em
+    height @width
+    background #ddd
+    position relative
+    border-radius 50%
+    .arrow
+      width 80%
+      height 80%
+      background rgba(255,255,255,.75)
+      border-radius 0 50% 50% 50%
+      transform rotate(45deg)
+</style>
