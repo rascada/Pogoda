eceb22cecd5afa34b6432d3d52ae6d0516607290
Damian <carftjr@gmail.com>
2015-12-15 21:44:56 +0100

    main color variable

--- src/Rascada/src/app.vue
+++ src/Rascada/src/app.vue
@@ -1,11 +1,12 @@
 <style lang="stylus">
 	@import url("http://fonts.googleapis.com/css?family=Rajdhani:400,600&amp;subset=latin,latin-ext");
+	@import "~styles/main"
 	@import "~styles/flex"
 
 	body
 		margin 1em 1.5em
 		overflow-x hidden
-		background #539DC2
+		background color
 		font-family 'Rajdhani', bold, sans-serif
 		main
 			display flex
