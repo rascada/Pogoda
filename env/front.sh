af6aa8881eebb9a0a1a090c23d33803ca8019d5f
Damian <rascada@users.noreply.github.com>
2015-12-16 21:51:38 +0100

    prod bash run webpack

--- env/front.sh
+++ env/front.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-node /usr/local/lib/node_modules/jade/bin/jade.js $DIR/../src/Rascada/views/jade/*.jade -o $DIR/../web
-node /usr/local/lib/node_modules/stylus/bin/stylus $DIR/../src/Rascada/views/stylus -o $DIR/../web/css
-node /usr/local/lib/node_modules/babel-cli/bin/babel.js $DIR/../src/Rascada/views/script --out-dir $DIR/../web/js
+node /usr/local/lib/node_modules/webpack $DIR/../src/Rascada
