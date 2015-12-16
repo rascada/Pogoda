67b3ad6b1986cf08dc4f87fa38fec7a6aadea3ac
marcinlacina <marcin.lacina@zslt.klucznet.pl>
2015-12-16 22:47:41 +0100

    fix: front.sh webpack exe file path

--- env/front.sh
+++ env/front.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-node /usr/local/lib/node_modules/webpack $DIR/../src/Rascada
+node /usr/local/lib/node_modules/webpack/bin/webpack.js $DIR/../src/Rascada
