1e337b5eb07420dd9eee37611c78001edb703894
Damian <carftjr@gmail.com>
2015-12-17 20:39:40 +0100

    create wind-section
    with compass

---
+++ src/Rascada/src/components/wind-section.vue
@@ -0,0 +1,25 @@
+<template lang='jade'>
+  compass(:direction='direction')
+</template>
+
+<script>
+  import compass from './compass.vue'
+
+  export default {
+    components: {
+      compass
+    },
+
+    data(){
+      return {
+        direction: 0
+      }
+    },
+
+    ready(){
+      this.$parent.api.basic.request.push(api => {
+        this.direction = api.wind.currentDir.value;
+      });
+    }
+  }
+</script>
