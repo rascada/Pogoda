ece0ec895ffcc5d77ef77e252c346cd6cb9572b7
Damian <carftjr@gmail.com>
2015-12-19 00:36:57 +0100

    wrap to new line

--- src/Rascada/src/app.vue
+++ src/Rascada/src/app.vue
@@ -1,17 +1,17 @@
 <style lang="stylus">
-	@import url("http://fonts.googleapis.com/css?family=Rajdhani:400,600&amp;subset=latin,latin-ext");
-	@import "~styles/main"
-	@import "~styles/flex"
-
-	body
-		margin 1em 1.5em
-		overflow-x hidden
-		background color
-		font-family 'Rajdhani', bold, sans-serif
-		main
-			@extend .center
-			justify-content space-around
-
+  @import url("http://fonts.googleapis.com/css?family=Rajdhani:400,600&amp;subset=latin,latin-ext");
+  @import "~styles/main"
+  @import "~styles/flex"
+  
+  body
+    margin 1em 1.5em
+    overflow-x hidden
+    background color
+    font-family 'Rajdhani', bold, sans-serif
+    main
+      @extend .center
+      flex-wrap wrap
+      justify-content space-around
 </style>
 
 <template lang="jade">
