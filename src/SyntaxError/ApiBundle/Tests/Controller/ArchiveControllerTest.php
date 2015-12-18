bb433ae9794c8e49797d2cb418db439fa5f78ee7
marcinlacina <marcin.lacina@zslt.klucznet.pl>
2015-12-18 18:53:24 +0100

    ArchiveController test create
    #37

--- src/SyntaxError/ApiBundle/Tests/Controller/ArchiveControllerTest.php
+++ src/SyntaxError/ApiBundle/Tests/Controller/ArchiveControllerTest.php
@@ -4,18 +4,22 @@ namespace SyntaxError\ApiBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DayControllerTest extends WebTestCase
+class ArchiveControllerTest extends WebTestCase
 {
-    public function testCharts()
+    /**
+     * @param $period
+     * @dataProvider periodProvider
+     */
+    public function testCharts($period)
     {
         $client = static::createClient();
         $params = ['outTemp', 'outHumidity', 'barometer', 'rain', 'rainRate', 'windSpeed', 'windDir', 'windGust', 'windGustDir'];
 
         foreach($params as $param) {
-            $crawler = $client->request('GET', '/api/day-charts/'.$param.".json");
+            $crawler = $client->request('GET', '/api/'.$period.'-charts/'.$param.".json");
             $this->assertTrue($client->getResponse()->getStatusCode() == 200);
 
-            $crawler = $client->request('GET', '/api/day-charts/'.$param.".json", [], [], [
+            $crawler = $client->request('GET', '/api/'.$period.'-charts/'.$param.".json", [], [], [
                 'HTTP_X-Requested-With' => 'XMLHttpRequest'
             ]);
             $this->assertTrue($client->getResponse()->getStatusCode() == 200);
@@ -23,17 +27,28 @@ class DayControllerTest extends WebTestCase
         }
     }
 
-    public function testRecords()
+    /**
+     * @param $period
+     * @dataProvider periodProvider
+     */
+    public function testRecords($period)
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/api/day-records');
+        $crawler = $client->request('GET', '/api/'.$period.'-records');
         $this->assertTrue($client->getResponse()->getStatusCode() == 200);
-        $crawler = $client->request('GET', '/api/day-records.json', [], [], [
+        $crawler = $client->request('GET', '/api/'.$period.'-records.json', [], [], [
             'HTTP_X-Requested-With' => 'XMLHttpRequest'
         ]);
         $this->assertTrue($client->getResponse()->getStatusCode() == 200);
         $this->assertEquals('application/json; charset=utf-8', $client->getResponse()->headers->get('Content-Type'));
 
+    }
+
+    public function periodProvider()
+    {
+        return [
+          ['day'], ['month'], ['year']
+        ];
     }
 }
