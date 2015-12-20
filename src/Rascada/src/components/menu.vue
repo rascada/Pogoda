2079ef635f45b4dd078b59d8c9f22547858127e0
Damian <carftjr@gmail.com>
2015-12-20 13:33:27 +0100

    style nav buttons

--- src/Rascada/src/components/menu.vue
+++ src/Rascada/src/components/menu.vue
@@ -23,4 +23,21 @@ nav
 
   button
     margin 0 .5em
+    padding .5em .75em
+    border .1em solid transparent
+
+    background transparent
+    border-radius .1em
+    cursor pointer
+    color color
+
+    transition 1s
+    &:hover
+      transition .2s
+      transform scale(1.1)
+      border .1em solid color
+      box-shadow 0 .1em .1em rgba(#000, .15)
+    &:active, &:focus
+      transform scale(.95)
+      outline none
 </style>
