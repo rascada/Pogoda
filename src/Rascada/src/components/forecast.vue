aea0d0cb5d4da3038ae5fdbc9560214023e60892
Damian <carftjr@gmail.com>
2015-12-20 19:33:26 +0100

    make forecast api request

--- src/Rascada/src/components/forecast.vue
+++ src/Rascada/src/components/forecast.vue
@@ -16,6 +16,21 @@
       }
     },
 
+    ready(){
+      aja().url(`https://pi.syntax-shell.me/api/wu/forecast.json`)
+        .on('success', res => {
+          let forecast = res.forecast;
+          let week = forecast.txt_forecast;
+
+          Object.assign(this, {
+            update: week.date,
+            week: week.forecastday,
+            simple: forecast.simpleforecast.forecastday
+          })
+        })
+        .go();
+    }
+  }
 </script>
 
 <style lang='stylus'>
