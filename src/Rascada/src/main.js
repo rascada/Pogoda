0d4d1541f7fe6323921eae80aa2255044ee2efd5
Damian <carftjr@gmail.com>
2015-12-23 12:29:21 +0100

    better directory tree

--- src/Rascada/src/main.js
+++ src/Rascada/src/main.js
@@ -1,14 +1,12 @@
 var Vue = require('vue');
 var App = require('./app.vue');
-var roundFilter = require('vue-filters/round');
-var slide = require('vue/transition/slide');
 
-Vue.filter('round', roundFilter);
-Vue.transition('slide', slide);
+Vue.filter('round', require('vue/filter/round'));
+Vue.transition('slide', require('vue/transition/slide'));
 
 new Vue({
   el: 'body',
   components: {
-    app: App
-  }
+    app: App,
+  },
 });
