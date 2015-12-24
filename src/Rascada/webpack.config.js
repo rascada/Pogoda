96748c5dd85cd519eb4cb49a5b266de8a24ed43e
Damian <carftjr@gmail.com>
2015-12-24 23:58:21 +0100

    transpile js files with babel6
    fix #54

--- src/Rascada/webpack.config.js
+++ src/Rascada/webpack.config.js
@@ -4,22 +4,35 @@ let webpack = require('webpack');
 
 module.exports = {
   entry: './src/main.js',
+
   output: {
     path: '../../web/js',
     publicPath: '/js/',
     filename: 'build.js',
   },
+
   plugins: [
     new webpack.ProvidePlugin({
       dyn: 'dynamics.js',
     }),
   ],
+
   module: {
     loaders: [
       {
         test: /\.vue$/,
         loader: 'vue',
       },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+      },
     ],
+  },
+
+  babel: {
+    presets: ['es2015', 'stage-0'],
+    plugins: ['transform-runtime'],
   },
 };
