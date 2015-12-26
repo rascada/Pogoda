7dd80e21f11e62f80058474ebc1ea04b5f4fed17
Damian <carftjr@gmail.com>
2015-12-26 20:49:14 +0100

    turn vue debug on
    dev only

--- src/Rascada/src/main.js
+++ src/Rascada/src/main.js
@@ -4,6 +4,9 @@ import App from './app.vue';
 Vue.filter('round', require('vue/filter/round'));
 Vue.transition('slide', require('vue/transition/slide'));
 
+if (process.env.NODE_ENV != 'production')
+  Vue.config.debug = true;
+
 new Vue({
   el: 'body',
   components: {
