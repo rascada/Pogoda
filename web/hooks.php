5a5643c48de0cc17c10c0e62ec1f86e8a2ee4d85
marcinlacina <marcin.lacina@zslt.klucznet.pl>
2015-12-26 13:53:28 +0100

    fix invalid payload property name

--- web/hooks.php
+++ web/hooks.php
@@ -11,7 +11,7 @@ if( $redis->exists('deploy_running') && $redis->get('deploy_running') == 'true' 
 } else {
     $payload = json_decode(file_get_contents('php://input'));
     $pathToProject = __DIR__."/..";
-    if($payload->refs == 'refs/heads/master') {
+    if($payload->ref == 'refs/heads/master') {
         $redis->set('deploy_running', 'true');
         echo `$pathToProject/env/hooks.sh > /dev/null 2>/dev/null &`.PHP_EOL;
         echo "Deploy running. Check status on ".$_SERVER['HTTP_HOST']."/hooks_stat.html";
