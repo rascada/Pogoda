b070463e99997239dcb10939ca46cea598496407
marcinlacina <marcin.lacina@zslt.klucznet.pl>
2015-12-27 11:27:28 +0100

    remove unnecessary methods from ArchiveRepository

--- src/SyntaxError/ApiBundle/Repository/ArchiveRepository.php
+++ src/SyntaxError/ApiBundle/Repository/ArchiveRepository.php
@@ -3,7 +3,6 @@
 namespace SyntaxError\ApiBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\NonUniqueResultException;
 use SyntaxError\ApiBundle\Tools\Uniter;
 use SyntaxError\ApiBundle\Entity\Archive;
 
@@ -61,48 +60,5 @@ class ArchiveRepository extends EntityRepository
             ->setParameter( 'from', $from->getTimestamp() )
             ->setParameter( 'to', $to->getTimestamp() )
             ->getQuery()->getResult();
-    }
-
-    /**
-     * @param $property
-     * @param bool|true $getMax
-     * @return mixed|null
-     */
-    public function findTodayRecord($property, $getMax = true)
-    {
-        $from = (new \DateTime('now'))->setTime(0,0,0)->getTimestamp();
-        $to = (new \DateTime('now'))->setTime(23,59,59)->getTimestamp();
-        try {
-            $record = $this->getEntityManager()->getRepository("SyntaxErrorApiBundle:Archive")->createQueryBuilder('a')
-                ->select("a.$property, a.dateTime")
-                ->where('a.dateTime BETWEEN :from AND :to')
-                ->andWhere("a.$property is not NULL")
-                ->setParameter('from', $from)->setParameter('to', $to)
-                ->orderBy("a.$property", $getMax ? 'desc' : 'asc')->setMaxResults(1)->getQuery()->getOneOrNullResult();
-        } catch(NonUniqueResultException $e) {
-            return null;
-        }
-        if(!$record) return null;
-        return $record;
-    }
-
-    /**
-     * @return float|null
-     */
-    public function findAvgWindToday()
-    {
-        $from = (new \DateTime('now'))->setTime(0,0,0)->getTimestamp();
-        $to = (new \DateTime('now'))->setTime(23,59,59)->getTimestamp();
-        $archives = $this->getEntityManager()->getRepository("SyntaxErrorApiBundle:Archive")->createQueryBuilder('a')
-            ->select('a.windGustDir')
-            ->where('a.dateTime BETWEEN :from AND :to')
-            ->setParameter('from', $from)->setParameter('to', $to)
-            ->getQuery()->getResult();
-        $sum = 0;
-        foreach($archives as $archive) {
-            $sum += $archive['windGustDir'];
-        }
-        if(!$sum) return null;
-        return $sum/count($archives);
     }
 }
