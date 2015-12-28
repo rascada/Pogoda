443e1f753f2c1f192a573fcba9b640d33b52c657
marcinlacina <marcin.lacina@zslt.klucznet.pl>
2015-12-28 09:22:17 +0100

    check current branch on prod: hooks.php

--- web/hooks.php
+++ web/hooks.php
@@ -11,11 +11,13 @@ if( $redis->exists('deploy_running') && $redis->get('deploy_running') == 'true' 
 } else {
     $payload = json_decode(file_get_contents('php://input'));
     $pathToProject = __DIR__."/..";
-    if($payload->ref == 'refs/heads/master') {
+    $headContent = trim(str_replace("ref: ", "", file_get_contents($pathToProject."/.git/HEAD")));
+
+    if($payload->ref == $headContent) {
         $redis->set('deploy_running', 'true');
         echo `$pathToProject/env/hooks.sh > /dev/null 2>/dev/null &`.PHP_EOL;
         echo "Deploy running. Check status on ".$_SERVER['HTTP_HOST']."/hooks_stat.html";
     } else {
-        echo "I'm master.";
+        echo "I'm $headContent.";
     }
 }
