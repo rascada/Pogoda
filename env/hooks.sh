f49aa081f8f9e31a47aef87b4be2eb83b98fd043
marcinlacina <marcin.lacina@zslt.klucznet.pl>
2015-12-30 23:22:47 +0100

    refactor hooks.sh

--- env/hooks.sh
+++ env/hooks.sh
@@ -11,13 +11,10 @@ echo $GITPULL >> $STAT
 echo "</h4>" >> $STAT
 
 echo "<h4>Front: " >> $STAT
-FRONT=`$DIR/front.sh`
-echo $FRONT | sed -e 's/rendered/<br>rendered/g' | \
-sed -e 's/compiled/<br>compiled/g' | sed -e "s/\[//g" | \
-sed -e "s/[0-9]m//g" | sed -e "s/[0-9]//g" >> $STAT
+`$DIR/front.sh 2>&1` >> $STAT
 echo "</h4>" >> $STAT
 
 echo "<h4>Cache: " >> $STAT
 php $DIR/../app/console cache:clear --env=prod >> $STAT
 echo "</h4>" >> $STAT
-redis-cli set deploy_running false
+redis-cli set deploy_running "`date`"
