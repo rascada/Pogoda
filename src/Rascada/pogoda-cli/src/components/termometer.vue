8e6732e5fa9e0ce14af2df338ef4d4a444847b82
Damian <carftjr@gmail.com>
2015-12-31 13:16:15 +0100

    jade-loader bug
    fix #61

--- src/Rascada/pogoda-cli/src/components/termometer.vue
+++ src/Rascada/pogoda-cli/src/components/termometer.vue
@@ -7,22 +7,20 @@
         v-for='n in range'
         v-bind:class='{boldLine: n % 5 == 0}')
           p(v-if='n % 5 == 0 || n == 0').
-            {{ degrees < 0 ? '-' : ''}}
-            {{ range - n }}
-
+            {{ range - n * isPositiveTemp() }}
     .sensor
-      .temperatureSensor.sensorVal(:style="{height: sensorVal }")
+      .temperatureSensor.sensorVal(v-bind:style="{height: sensorVal }")
         span {{ degrees | round }}°C
     .bottom
 </template>
 
 <script>
   export default {
-    data () {
+    data() {
       return {
         degrees: 0,
         range: 30,
-      }
+      };
     },
 
     computed: {
@@ -31,119 +29,124 @@
       },
     },
     methods: {
-      updateTemperature(){
+      updateTemperature() {
         this.$parent.basic('temperature')
           .on('success', res => {
             this.degrees = res.temperature.current.value;
             setTimeout(this.updateTemperature, res.time.next.value * 1000);
           }).go();
       },
-      apiConnect(api){
+
+      apiConnect(api) {
         this.degrees = api.temperature.current.value;
+      },
+
+      isPositiveTemp() {
+        return this.degrees < 0 ? -1 : 1;
       }
     },
 
-    ready () {
+    ready() {
       this.$parent.api.basic.on('updated', this.apiConnect);
     },
 
-    destroyed () {
+    destroyed() {
 
     }
   }
 </script>
 
 <style lang='stylus'>
-@import "~styles/main"
-thermometerColor = #f42
+  @import "~styles/main"
+  thermometerColor = #f42
 
-.tempSect
-  @extend .sect, .blockShadow
+  .tempSect
+    @extend .sect, .blockShadow
 
-.thermometer
-  width 4.5em
-  height 24em
-  display flex
-  padding .5em
-  padding-top 2em
-  background #fff
-  border-radius 4em
-  margin-bottom 6em
-  position relative
-  justify-content center
-  .bottom
-    top 90%
-    width 35%
-    height 32%
-    content ''
+  .thermometer
+    width 4.5em
+    height 24em
+    display flex
+    padding .5em
+    padding-top 2em
     background #fff
-    position absolute
-    border-radius 2em
-    overflow hidden
-    left 50% - (@width/2)
-  &:after
-    content ''
-    position absolute
-    top -1.9%
-    left 56%
-    width 110%
-    height 135%
-    z-index 3
-    transform skew(25deg) rotate(25deg)
-    background rgba(0,0,0,.1)
-  .sensor
-    z-index 2
-    width 15%
-    height 107.5%
+    border-radius 4em
+    margin-bottom 6em
     position relative
-    borderRound()
-    background #eee
-    &:before
-      top 97%
-      width 150%
-      height 18%
+    justify-content center
+    .bottom
+      top 90%
+      width 35%
+      height 32%
       content ''
-      background thermometerColor
+      background #fff
       position absolute
       border-radius 2em
-      left 49.5% - (@width/2)
-    .sensorVal
+      overflow hidden
+      left 50% - (@width/2)
+    &:after
+      content ''
       position absolute
-      bottom 0
-      left 0
-      width 100%
-      height 70%
-      background thermometerColor
-      span
-        background #fff
-        padding .2em
-        box-shadow 0 .1em .1em rgba(#000, .1)
-        border .05em solid thermometerColor
-        color thermometerColor
-        font-weight 600
-        font-size 1.25em
-        position relative
-        top -7.5%
-        left 150%
-  .measure
-    position absolute
-    height 85%
-    width 20%
-    z-index 2
-    left 0
-    display flex
-    flex-flow column
-    .line
+      top -1.9%
+      left 56%
+      width 110%
+      height 135%
+      z-index 3
+      transform skew(25deg) rotate(25deg)
+      background rgba(0,0,0,.1)
+    .sensor
+      z-index 2
+      width 15%
+      height 107.5%
       position relative
-      background #444
-      flex-grow 1
-      margin-top .5em
-      width 90%
-    .boldLine
-      width 150%
-      p
+      borderRound()
+      background #eee
+      &:before
+        top 97%
+        width 150%
+        height 18%
+        content ''
+        background thermometerColor
         position absolute
-        right 110%
-        top -200%
-        margin 0
+        border-radius 2em
+        left 49.5% - (@width/2)
+      .sensorVal
+        position absolute
+        bottom 0
+        left 0
+        width 100%
+        height 70%
+        background thermometerColor
+        span
+          background #fff
+          padding .2em
+          box-shadow 0 .1em .1em rgba(#000, .1)
+          border .05em solid thermometerColor
+          color thermometerColor
+          font-weight 600
+          font-size 1.25em
+          position relative
+          top -7.5%
+          left 150%
+    .measure
+      position absolute
+      height 85%
+      width 20%
+      z-index 2
+      left 0
+      display flex
+      flex-flow column
+      .line
+        position relative
+        background #444
+        flex-grow 1
+        margin-top .5em
+        width 90%
+      .boldLine
+        width 150%
+        p
+          position absolute
+          right 110%
+          top -200%
+          margin 0
 </style>
