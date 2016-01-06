fbb6033c2a63f88e5bac9c7978686eae7f194514
Damian <carftjr@gmail.com>
2016-01-07 00:30:04 +0100

    next-update component

---
+++ src/Rascada/pogoda-cli/src/components/nextUpdate.vue
@@ -0,0 +1,24 @@
+<template lang="jade">
+span aktualizacja za {{timeToUpdate}}s
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      timeToUpdate: 0,
+    };
+  },
+
+  ready() {
+    this.$parent.$parent.api.basic.on('nextUpdate', time => this.nextUpdate(time));
+  },
+
+  methods: {
+    nextUpdate(time) {
+      setTimeout(_ => this.nextUpdate(--time), 1000);
+      return this.timeToUpdate = time;
+    },
+  },
+}
+</script>
