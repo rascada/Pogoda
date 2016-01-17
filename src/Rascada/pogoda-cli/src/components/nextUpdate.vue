4c9160e09b4a29495f7a09cd3b2870474856224e
rascada <carftjr@gmail.com>
2016-01-17 13:01:24 +0100

    refactor updateTime
    also info about updating

--- src/Rascada/pogoda-cli/src/components/nextUpdate.vue
+++ src/Rascada/pogoda-cli/src/components/nextUpdate.vue
@@ -1,6 +1,6 @@
 <template lang="jade">
 
-span aktualizacja za {{timeToUpdate}}
+span {{updateInfo}}
 
 </template>
 
@@ -10,24 +10,30 @@ import ms from 'pretty-ms';
 export default {
   data() {
     return {
-      timeToUpdate: 0,
+      updateInfo: 'aktualizowanie',
     };
   },
 
   ready() {
-    this.$parent.$parent.api.basic.on('nextUpdate', time => this.nextUpdate(time));
+    this.$parent.$parent.api.basic
+      .on('nextUpdate', time => this.nextUpdate(time));
   },
 
   methods: {
     nextUpdate(time) {
       if (time) {
         this.thread ? clearTimeout(this.thread) : null;
-        this.thread = setInterval(_ => {
-          this.timeToUpdate = ms(--time * 1000);
-        }, 1000);
+
+        this.thread = setInterval(_ => this.parse(--time), 1000);
       }
       return time;
     },
+
+    parse(time) {
+      this.updateInfo = time
+        ? `aktualizacja za ${ ms(time * 1000) }`
+        : 'aktualizowanie';
+    }
   },
 }
 </script>
